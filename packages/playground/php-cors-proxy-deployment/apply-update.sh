faea1ee10ec4921a30a7922ce97cd51d7cbec5f6
Brandon Payton <brandon@happycode.net>
2024-12-03 19:32:25 -0500

    Fix CORS proxy deploy workflow (#2049)
    ## Motivation for the change, related issues
    
    The CORS proxy deploy workflow isn't able to find the files it checked
    out in order to sync them to the dedicated proxy site.
    
    ## Implementation details
    
    This PR fixes multiple incorrect paths, tightens command line params,
    and restores some missing bash var declarations in order to fix
    deployment of the dedicated CORS proxy.
    
    ## Testing Instructions (or ideally a Blueprint)
    
    Allow dispatching this branch's version of the workflow and trigger the
    workflow manually to test.

--- packages/playground/php-cors-proxy-deployment/apply-update.sh
+++ packages/playground/php-cors-proxy-deployment/apply-update.sh
@@ -2,6 +2,14 @@
 
 set -euo pipefail
 
+# Explicitly use the site's declared PHP version.
+# Otherwise, we've seen this defaulting to older versions which can break our scripts.
+SITE_PHP="/usr/local/php${PHP_VERSION}/bin/php"
+SITE_API_BASE="$( "$SITE_PHP" -r 'require "/scripts/env.php"; echo SITE_API_BASE;')"
+
+echo Adding config file to updated proxy files
+cp ~/cors-proxy-deployment/cors-proxy-config.php ~/updated-proxy-files/
+
 echo Syncing staged files to production
 rsync -av --delete --no-perms --omit-dir-times ~/updated-proxy-files/ /srv/htdocs/
 
@@ -14,6 +22,6 @@ curl -sS -X POST -H "Auth: $ATOMIC_SITE_API_KEY" "$SITE_API_BASE/edge-cache/$ATO
 echo Applying latest CORS proxy rate-limiting schema
 # NOTE: This will reset rate-limiting token buckets, but that should be tolerable
 # as long as we're generally discouraging abuse of the proxy.
-cat ~/website-deployment/cors-proxy-rate-limiting-table.sql | mysql --database="$DB_NAME"
+cat ~/cors-proxy-deployment/cors-proxy-rate-limiting-table.sql | mysql --database="$DB_NAME"
 
 echo Done!
